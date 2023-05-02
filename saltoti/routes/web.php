ba8a52818188ecb3a309baebd2d5a6d18afb2d5f
KiKiHUN <49228599+KiKiHUN@users.noreply.github.com>
2023-05-02 15:31:19 +0200

    edit

--- saltoti/routes/web.php
+++ saltoti/routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\DB;
 Route::get('/', function () {
     try {
         DB::connection()->getPdo();
-        dd("müxik");
+        print_r("müxik");
     } catch (\Exception $e) {
-        die("Could not connect to the database.  Please check your configuration. error:" . $e );
+        print_r("Could not connect to the database.  Please check your configuration. error:" . $e );
     }
     return view('welcome');
 });
